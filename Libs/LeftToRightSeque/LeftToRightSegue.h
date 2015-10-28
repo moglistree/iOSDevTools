e389e805111b8f366a7aa5f6879845f4937c8429
Martin <martin.andonoski@spoiledmilk.com>
2015-10-28 11:35:33 +0100

    left to right segue added

---
+++ Libs/LeftToRightSeque/LeftToRightSegue.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface LeftToRightSegue : UIStoryboardSegue
+
+@end
---
+++ Libs/LeftToRightSeque/LeftToRightSegue.m
@@ -0,0 +1,25 @@
+#import "LeftToRightSegue.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation LeftToRightSegue
+
+-(void)perform {
+    
+    __block UIViewController *sourceViewController = (UIViewController*)[self sourceViewController];
+    __block UIViewController *destinationController = (UIViewController*)[self destinationViewController];
+    
+    CATransition* transition = [CATransition animation];
+    transition.duration = .25;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    transition.type = kCATransitionPush; //kCATransitionMoveIn; //, kCATransitionPush, kCATransitionReveal, kCATransitionFade
+    transition.subtype = kCATransitionFromLeft; //kCATransitionFromLeft, kCATransitionFromRight, kCATransitionFromTop, kCATransitionFromBottom
+    
+    [sourceViewController.navigationController.view.layer addAnimation:transition
+                                                                forKey:kCATransition];
+    
+    [sourceViewController.navigationController pushViewController:destinationController animated:NO];
+    
+    
+}
+
+@end
